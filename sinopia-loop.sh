a79a8af741e91265897eec57cb3dab86318ff0a2
Alexander Ma[ẏ]orov <alexander.majorov@gmail.com>
2016-08-03 16:12:10 +0300

    Update sinopia-loop.sh

--- sinopia-loop.sh
+++ sinopia-loop.sh
@@ -6,14 +6,16 @@ CMD="sinopia"
 
 echo $$ > sinopia.pid
 
-while true; do
+while true
+do
   echo >> $OUTFILE
   echo >> $ERRFILE
   date >> $OUTFILE
   date >> $ERRFILE
   echo "Starting $CMD" >> $OUTFILE
   echo "Starting $CMD" >> $ERRFILE
-  $CMD 1>>$OUTFILE 2>>$ERRFILE
+  #$CMD 1>>$OUTFILE 2>>$ERRFILE
+  $CMD 1>>/dev/null 2>>$ERRFILE
 done
 
 #EOF#
